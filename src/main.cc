80144a4678b22faef4a871e641f2ba7afbc9b3d7
iceseyes <bianchi.massimo@gmail.com>
2017-03-15 23:35:01 +0100

    Create a basic main.cc and a basic test. Setup CMakelists, Git and test eclipse

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+/**
+ * @file: main.cc
+ *
+ *  Created on: 15 mar 2017
+ *      Author: massimo Bianchi
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
+
+
+
+
