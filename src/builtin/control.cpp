4855d73a29de39fc7d2a02d9257ab82e09546b75
iceseyes <bianchi.massimo@gmail.com>
2017-08-22 23:03:39 +0200

    fix repeat built-in operation.

--- src/builtin/control.cpp
+++ src/builtin/control.cpp
@@ -19,11 +19,17 @@ struct Repeat : BuiltinProcedure {
         int arg0 = fetchArg(0).asUnsigned();
         string arg1 = fetchArg(1).toString();
 
-        for(int i=0; i<arg0; ++i) {
-            auto stmt = parser::parse(arg1);
-            auto ast = eval::make_ast(stmt);
+        // arg1 is something like [...] where
+        // ... maybe empty string or a very complex
+        // expression. We have to take only string into brackets (like arg1[1:-1])
+        // for parsing, so check if arg1.size() is at least 2 ([])
+        if(arg1.size()>2) {
+            for(int i=0; i<arg0; ++i) {
+                auto stmt = parser::parse(arg1.substr(1, arg1.size()-2));
+                auto ast = eval::make_ast(stmt);
 
-            ast();
+                ast();
+            }
         }
     }
 };
